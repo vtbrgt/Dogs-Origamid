016f81b59f6ec7e08e561b7d11cacbe70082eb11
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-01-25 10:45:57 -0300

    testing API

---
+++ src/api/endpoints/PhotoPost.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const PhotoPost = () => {
+  // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvZG9nc2FwaS5vcmlnYW1pZC5kZXYiLCJpYXQiOjE2NzQ2NTMzNzAsIm5iZiI6MTY3NDY1MzM3MCwiZXhwIjoxNjc0NzM5NzcwLCJkYXRhIjp7InVzZXIiOnsiaWQiOiIxNSJ9fX0.PshH2QIk4Xx0YZHa6TTUMSLRhZmzsvHTs0LvDoel8e0
+
+  const [token, setToken] = React.useState('');
+  const [nome, setNome] = React.useState('');
+  const [peso, setPeso] = React.useState('');
+  const [idade, setIdade] = React.useState('');
+  const [img, setImg] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('nome', nome);
+    formData.append('peso', peso);
+    formData.append('idade', idade);
+    formData.append('img', img);
+
+    fetch('https://dogsapi.origamid.dev/json/api/photo', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+      body: formData,
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        return json;
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="token"
+        value={token}
+        onChange={({ target }) => setToken(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="nome"
+        value={nome}
+        onChange={({ target }) => setNome(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="peso"
+        value={peso}
+        onChange={({ target }) => setPeso(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="idade"
+        value={idade}
+        onChange={({ target }) => setIdade(target.value)}
+      />
+      <input type="file" onChange={({ target }) => setImg(target.files[0])} />
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default PhotoPost;
