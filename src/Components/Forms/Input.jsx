21c5f3103e6fa802df97a884ada6befeee0f8228
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-01-31 09:29:50 -0300

    creating custom hook useForm

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} name={name} type={type} className={styles.input} />
-      <p className={styles.error}>Erro</p>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        className={styles.input}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
