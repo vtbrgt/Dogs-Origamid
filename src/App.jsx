24dfae50226e631486e602484018000a4ed48a09
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-02-06 19:34:28 -0300

    creating a protected route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import User from './Components/User/User';
 import './App.css';
 
 function App() {
@@ -15,7 +17,15 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
