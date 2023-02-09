684da925e22eb0a418ba10ac456d86ae5d423411
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-02-09 10:39:14 -0300

    feed modal 2

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -6,8 +6,12 @@ import Loading from '../Helper/Loading';
 import PhotoContent from '../Photo/PhotoContent';
 import styles from './FeedModal.module.css';
 
-const FeedModal = ({ photo }) => {
+const FeedModal = ({ photo, setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
 
   React.useEffect(() => {
     const { url, options } = PHOTO_GET(photo.id);
@@ -15,7 +19,7 @@ const FeedModal = ({ photo }) => {
   }, [photo, request]);
 
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
