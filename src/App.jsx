14d53f7811a95a73184210d53ab01fdefafb66cc
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-03-14 11:07:42 -0300

    creating single photo page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { UserStorage } from './UserContext';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import User from './Components/User/User';
 import './App.css';
+import Photo from './Components/Photo/Photo';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route path="foto/:id" element={<Photo />} />
           </Routes>
           <Footer />
         </UserStorage>
